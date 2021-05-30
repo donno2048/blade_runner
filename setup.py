7ad8774c81ca8392d0559e04a73ee2ddda1fa664
Elisha Hollander <just4now666666@gmail.com>
2021-05-30 16:42:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 setup(
     name='blade_runner',
     include_package_data=True,
-    version='1.1.2',
+    version='1.1.3',
     packages=find_packages(),
     package_data={'blade_runner': ['blade_runner'],},
     url = 'https://github.com/donno2048/blade_runner',
